2b18be952dcc73b4bcf596ba0e0169ff1d60fcc5
mehmeteminburakoksuz <meboksuz02@gmail.com>
2024-07-03 17:16:55 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", window.scrollY > 130);
+});
+
+let menu = document.querySelector('#menu-icon');
+let menulist  = document.querySelector('#menulist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    menulist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    menulist.classList.remove('open');
+};
+
+var typed = new Typed(".input",{
+    strings:["Mobile App","Backend Developer","Web3","Crypto"],
+    typeSpeed: 120,
+    backSpeed: 70,
+    loop:true
+});
+
+
